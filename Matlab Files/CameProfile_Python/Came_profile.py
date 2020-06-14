9565c3da4a0ef00c6687bed6bb4012dbd64e7764
Anderson <andersontb8@gmail.com>
2020-06-14 12:47:44 -0300

    FEATURE / geometry_generation function

--- Matlab Files/CameProfile_Python/Came_profile.py
+++ Matlab Files/CameProfile_Python/Came_profile.py
@@ -3,6 +3,7 @@ Code to determine the Came profile
 """
 import numpy as np
 import matplotlib.pyplot as plt 
+import math
 
 # GEOMETRICAL PARAMETERS 
 l0 = 27.5   # Length from the bearing to the hinge 
@@ -11,8 +12,8 @@ hb = 4.0    # Length of the vertical support of the bearing
 br = 1.1    # Bearing radius 
 
 # max and min positions of the bearing
-h1a = np.array([6.5, 7.1, 7.7, 8.3, 8.9])
-h2a = np.array([20.5, 20.5, 20.5, 20.5, 20.5])
+h1a = [6.5, 7.1, 7.7, 8.3, 8.9]
+h2a = [20.5, 20.5, 20.5, 20.5, 20.5]
 
 nc = 1    # Cycles per cam turn (1 to 3)
 rmin = 5  # Minimum radius of the camshaft 
@@ -37,12 +38,40 @@ ff2 = 50
 #####################################################
 # GENERATION OF THE CURVES AND THE CAMSHAFT
 
+ 
+def geometry_generation(h1a, h2a, h0, hb):
+    
+    l = math.sqrt(l0**2 + hb**2)
 
-def quadrado(num):
-    return num ** 2
+    ymin = h1a
+    ymax = h2a
 
-print(quadrado(h1a))
+    alphamin = math.acos((h0 - h1a) / l)
+    alphamax = math.acos((h0 - h2a) / l)
 
+    xmin = l*math.sin(alphamin)
+    xmax = l*math.sin(alphamax)
+
+    d = math.sqrt((xmin - xmax) ** 2 + (ymin - ymax) ** 2)
+    alphatan = (alphamin + alphamax) / 2
+
+    xtan = l*math.sin(alphatan)    
+    ytan = h0 - l*math.cos(alphatan)
+
+    xsup = xtan + (d/2)*math.cos(alphatan)
+    xinf = xtan - (d/2)*math.cos(alphatan)
+    
+    ysup = ytan + (d/2)*math.sin(alphatan)
+    yinf = ytan - (d/2)*math.sin(alphatan)
+    
+    xcam = xtan + (d/2 + rmin + br)*math.cos(alphatan)
+    ycam = ytan + (d/2 + rmin + br)*math.sin(alphatan)
+    
+    return [xtan, ytan]
+
+a = geometry_generation(h1a[1], h2a[1], h0, hb)
+print(a)
+print(type(a))
 
 
 
